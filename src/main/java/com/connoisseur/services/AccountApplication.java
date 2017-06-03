f7996764c6c4ee6f819207a9d6be4d4520d6185a
Yu Chen <ychennay@gmail.com>
2017-06-03 13:36:20 -0700

    added in eureka server discovery client

--- src/main/java/com/connoisseur/services/AccountApplication.java
+++ src/main/java/com/connoisseur/services/AccountApplication.java
@@ -3,13 +3,14 @@ package com.connoisseur.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @SpringBootApplication
 @EnableAutoConfiguration
-
+@EnableDiscoveryClient
 @Import({springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration.class})
 public class AccountApplication  {
 
